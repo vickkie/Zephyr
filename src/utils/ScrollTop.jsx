1ae9ea3cafc2a3f33461efa516d1d338a12ac672
vickkie <vickkietrake@gmail..com>
2025-01-26 07:29:04 -0500

    scrolltopfix

--- src/utils/ScrollTop.jsx
+++ src/utils/ScrollTop.jsx
@@ -3,8 +3,15 @@ import { useLocation } from "react-router-dom";
 
 export default function ScrollTop() {
   const location = useLocation();
+
+  const exemptions = ["shop", "women", "men", "kids"];
+
   useEffect(() => {
-    window.scrollTo(0, 0);
+    if (exemptions.includes(location.pathname.split("/")[1], 0)) {
+      // console.log("seen");
+    } else {
+      window.scrollTo(0, 0);
+    }
   }, [location]);
   return null;
 }
