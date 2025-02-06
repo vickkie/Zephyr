42aa439f9ebce4de6490ac90ca2837f77341e734
vickkie <vickkietrake@gmail..com>
2025-02-06 14:11:17 -0500

    featured

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { Header, Hero, CallToAction, Features, Footer, ProductSlider } from "../components";
+import LatestProducts from "../components/LatestProducts";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Hero />
+      <LatestProducts />
       <Features />
       <ProductSlider />
       <CallToAction />
